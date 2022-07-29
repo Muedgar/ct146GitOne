7ec435eb6254245f2ab3ee985c4e3b53f524fcd5
Muedgar <edgarmutangana@gmail.com>
2022-07-29 10:39:20 +0200

    update

--- ctserver/docs/servers.js
+++ ctserver/docs/servers.js
@@ -1,10 +1,6 @@
 module.exports = {
     servers:[
         {
-            url: "http://localhost:3001/",
-            description: "Local server"
-        },
-        {
             url: "https://ct146.herokuapp.com/",
             description: "Remote server"
         }
